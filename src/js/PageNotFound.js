1ac5e68141dea2b4e64f40b7e269ec5e889ae305
Zayn Jarvis <zaynjarvis@wifi-131-179-3-53.host.ucla.edu>
2018-08-08 17:32:41 -0700

    router

---
+++ src/js/PageNotFound.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div id="contact" className="content">
+        <div className="page-not-found">
+          <div>
+            <h1 className="lg-heading">Page Not Found.</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
