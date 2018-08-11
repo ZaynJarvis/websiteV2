d775d5537b4a318b726ec7b53ac1a233222d89c0
Zayn Jarvis <zaynjarvis@gmail.com>
2018-08-11 11:57:17 -0700

    add server

--- src/js/Calendar.js
+++ src/js/Calendar.js
@@ -18,7 +18,7 @@ class Calendar extends Component {
     // get the data, transfer to result
     const result = generateJSON(this.state.value);
     // send the result to the server
-    fetch("https://localhost:443/api/store", {
+    fetch("https://localhost:4430/api/store", {
       method: "POST",
       headers: {
         "Content-Type": "application/json; charset=utf-8"
