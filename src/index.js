93b12ccbd8f76921509fdedc031e34e67037aa22
Zayn Jarvis <zaynjarvis@gmail.com>
2018-08-14 02:30:53 +0800

    stability test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Main from "./js/components/Main";
+import Main from "./js/Main";
 import "./css/main.css";
 import registerServiceWorker from "./registerServiceWorker";
 registerServiceWorker();
