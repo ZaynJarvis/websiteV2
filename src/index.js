28bf91b6620f3946a84516ec92c59cb5806f05fb
Zayn Jarvis <zaynjarvis@s-169-232-119-130.resnet.ucla.edu>
2018-08-12 21:11:59 -0700

    delete service worker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./js/Main";
 import "./css/main.css";
-import registerServiceWorker from "./registerServiceWorker";
-
+import { unregister } from "./registerServiceWorker";
+unregister();
 ReactDOM.render(<Main router="home" />, document.querySelector("#root"));
-
-registerServiceWorker();
