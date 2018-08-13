d6c6fc5b9de3fec4ef8843490d7324f1cd643f02
Zayn Jarvis <zaynjarvis@s-169-232-119-130.resnet.ucla.edu>
2018-08-14 02:32:10 +0800

    fix

--- src/js/PageNotFound.js
+++ src/js/PageNotFound.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-class Contact extends Component {
+class PageNotFound extends Component {
   render() {
     return (
       <div id="contact" className="content">
@@ -14,4 +14,4 @@ class Contact extends Component {
   }
 }
 
-export default Contact;
+export default PageNotFound;
