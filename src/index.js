23eb0c8f6e4afcf393cfb99985bff7e06f63e2fc
Zayn Jarvis <zaynjarvis@s-169-232-119-130.resnet.ucla.edu>
2018-08-12 20:59:53 -0700

    add service worker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./js/Main";
 import "./css/main.css";
-// import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<Main router="home" />, document.querySelector("#root"));
 
-// registerServiceWorker();
+registerServiceWorker();
