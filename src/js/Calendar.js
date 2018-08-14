71cdbcdd30be203660b5f4a8d643f000f0e83726
Zayn Jarvis <zaynjarvis@gmail.com>
2018-08-14 01:40:01 +0000

    post from rm

--- src/js/Calendar.js
+++ src/js/Calendar.js
@@ -57,7 +57,7 @@ class Calendar extends Component {
               <input
                 type="submit"
                 className="generate"
-                value="Testing Mode"
+                value="Click to Generate"
               />
             </form>
           </div>
