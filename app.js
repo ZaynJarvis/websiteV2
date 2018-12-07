c2fa55bbae01351f7facfc6a7983f9588bd6808e
Zayn Jarvis <ZaynJarvis@gmail.com>
2018-12-07 10:29:00 +0800

    fix span to .number

--- app.js
+++ app.js
@@ -9,6 +9,24 @@ const express = require("express"),
 const Mongo = require("./mongo");
 const User = Mongo.User;
 const Info = Mongo.Info;
+const rp = require("request-promise");
+const $ = require("cheerio");
+
+app.get("/version/:name", async (req, res) => {
+  const html = await rp(`https://img.shields.io/pub/v/${req.params.name}.svg`);
+  res.send(html);
+});
+
+app.get("/score/:name", async (req, res) => {
+  const html = await rp(`https://pub.dartlang.org/packages/${req.params.name}`);
+
+  $(".score-box > .number", html).each(async function() {
+    const badge = await rp(
+      `https://img.shields.io/badge/pub.score-${$(this).text()}-blue.svg`
+    );
+    res.send(badge);
+  });
+});
 
 // HTTPS setup
 const options = {
